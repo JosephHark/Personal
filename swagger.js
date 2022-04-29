9a014d8e4e00db8618680b9501c877afe2871c0e
JosephHark <har17033@byui.edu>
2022-04-29 19:03:24 -0400

    one

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Temple API'
     },
-    host: 'localhost:8080',
+    host: 'https://glacial-savannah-83570.herokuapp.com/',
     schemes: ['https']
 };
 
